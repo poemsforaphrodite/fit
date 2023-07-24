0a32f2e78bda23376253f623802801109824a827
zyneman <pushpendersolanki_co21a6_23@dtu.ac.in>
2023-07-24 07:53:14 +0530

    ?

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -12,7 +12,7 @@ function Login({ onLogin }) {
     e.preventDefault();
 
     try {
-      const response = await axios.post("http://localhost:8000/login", {
+      const response = await axios.post("http://thera1.com/Login", {
         email,
         password,
       });
