7fca5068bcaf2952a39f9d1fff45a560908b49c6
zyneman <pushpendersolanki_co21a6_23@dtu.ac.in>
2023-07-28 00:14:40 +0530

    access

--- server/index.js
+++ server/index.js
@@ -49,7 +49,12 @@ const cors = require("cors");
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "https://example.com");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+})
 
 app.get("/", cors(), (req, res) => {
   res.send("Hello Wossrldsssssss");
