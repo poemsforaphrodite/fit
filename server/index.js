abd8f65c56c2fbe87878499d0e80392ec27e2ed6
zyneman <pushpendersolanki_co21a6_23@dtu.ac.in>
2023-07-27 09:10:28 +0530

    1

--- server/index.js
+++ server/index.js
@@ -11,8 +11,40 @@ const stripe = require("stripe")(
 const openai = new OpenAIApi(configuration);
 require("dotenv").config({ path: path.join(__dirname, "../.env") });
 console.log("JWT_SECRET:", process.env.JWT_SECRET);
-const User = require("../src/mongo"); // Updated import
 const mongoose = require("mongoose");
+
+mongoose
+  .connect(
+    "mongodb+srv://pushpendersolanki895:1234@cluster0.blspoof.mongodb.net/Cluster0?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("Connected to MongoDB..."))
+  .catch((err) => console.error("Could not connect to MongoDB..."));
+
+
+const newSchema = new mongoose.Schema({
+  _id: { type: mongoose.Schema.Types.ObjectId, auto: true }, // Add this line
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  age: { type: Number },
+  weight: { type: Number },
+  height: { type: Number },
+  fitnessGoals: { type: String },
+  therapistId: { type: Number }, // Add this line
+  appointmentDate: { type: Date }, // Add this line
+  appointmentTime: { type: String }, // Add this line
+  status: { type: String, default: "pending" },
+  workoutPlan: { type: String },
+  zoomMeetingId: { type: String },
+  zoomMeetingJoinUrl: { type: String },
+  temp: {
+    meow: { type: String },
+    meow2: { type: String },
+  }
+});
+
+const User = mongoose.model("User", newSchema);
+
 const jwt = require("jsonwebtoken");
 const cors = require("cors");
 const app = express();
