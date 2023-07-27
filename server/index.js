ab181eb7113c6d2ef867681c3dca97709e5af397
zyneman <pushpendersolanki_co21a6_23@dtu.ac.in>
2023-07-28 00:17:56 +0530

    oops

--- server/index.js
+++ server/index.js
@@ -50,7 +50,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://example.com");
+  res.setHeader("Access-Control-Allow-Origin", "https://thera1.com");
   res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   next();
